50ed3b566f5aef19a67cfd62c8edfa2727572d83
Appile1 <ahmadsiddiquej3@gmail.com>
2024-01-10 10:04:27 +0500

    api issue resolved

--- indes.js
+++ indes.js
@@ -3,10 +3,13 @@ let modeSelect = document.getElementById("colors");
 let genBtn = document.getElementById("generate-color");
 
 genBtn.addEventListener("click", () => {
+  let inputColor = colorInput.value.replace(/^#/, "");
   let selectedMode = modeSelect.value;
   fetch(
-    `https://www.thecolorapi.com/scheme?rgb=${colorInput.value}&mode=${selectedMode}&count=6&format=json`
+    `https://www.thecolorapi.com/scheme?hex=${inputColor}&mode=${selectedMode}&count=6&format=json`
   )
     .then((res) => res.json())
-    .then((data) => console.log(data));
+    .then((data) => {
+      console.log(data);
+    });
 });
