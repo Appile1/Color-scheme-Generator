d9b66de830beac6e5a22ee087d65423d99c0e1ae
Appile1 <ahmadsiddiquej3@gmail.com>
2024-01-10 10:09:47 +0500

    almost completed

--- indes.js
+++ indes.js
@@ -2,14 +2,23 @@ let colorInput = document.getElementById("color");
 let modeSelect = document.getElementById("colors");
 let genBtn = document.getElementById("generate-color");
 
+// Function to update the background color of a div
+function updateDivColor(divId, color) {
+  document.getElementById(divId).style.backgroundColor = color;
+}
+
 genBtn.addEventListener("click", () => {
   let inputColor = colorInput.value.replace(/^#/, "");
   let selectedMode = modeSelect.value;
+
   fetch(
     `https://www.thecolorapi.com/scheme?hex=${inputColor}&mode=${selectedMode}&count=6&format=json`
   )
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
+      // Extract colors from the response and update divs
+      data.colors.forEach((color, index) => {
+        updateDivColor(`color-show-${index + 1}`, color.hex.value);
+      });
     });
 });
